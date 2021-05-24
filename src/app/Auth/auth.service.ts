979a73616eb3ac9d66037202bb5e0d9b911c0c68
RASMAP <ras.seidi65@gmail.com>
2021-05-23 21:27:53 -0700

    .

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -11,22 +11,22 @@ export class AuthService {
   }
 
   registerUser(data: any): Observable<any> {
-    return this.http.post('http://api.jahantebkhoram.ir/api/v1/user/registerUser', data);
+    return this.http.post('http://api.sorenabook.ir/api/v1/user/register', data);
   }
 
   loginUser(data: any): Observable<any> {
-    return this.http.post('http://api.jahantebkhoram.ir/api/v1/user/loginUser', data);
+    return this.http.post('http://api.sorenabook.ir/api/v1/user/login', data);
   }
 
   resetPasswordUser(data: any) {
-    return this.http.post('http://api.jahantebkhoram.ir/api/v1/user/resetPassword', data);
+    return this.http.post('http://api.sorenabook.ir/api/v1/user/resetPassword', data);
   }
 
   changePasswordUser(id: any) {
-    return this.http.get('http://api.jahantebkhoram.ir/api/v1/user/changePassword/' + id);
+    return this.http.get('http://api.sorenabook.ir/api/v1/user/changePassword/' + id);
   }
 
   getUser(id: any) {
-    return this.http.get('http://api.jahantebkhoram.ir/api/v1/user/getUser/' + id);
+    return this.http.get('http://api.sorenabook.ir/api/v1/user/index/' + id);
   }
 }
--- src/app/layout/home/cart/cart.component.ts
+++ src/app/layout/home/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {CartService} from '../../../serviceCart/cart.service';
+import {LocalStorageService} from '../../../Auth/localStorageLogin/local-storage.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,7 +12,10 @@ export class CartComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   isEditable = false;
-  constructor(private _formBuilder: FormBuilder) {
+  items: any[];
+  constructor(private _formBuilder: FormBuilder,
+              private cart:CartService,
+              private localstorage:LocalStorageService) {
   }
 
   ngOnInit(): void {
@@ -21,5 +26,33 @@ export class CartComponent implements OnInit {
       secondCtrl: ['']
     });
   }
-
+  deleteCart(item: any) {
+    this.cart.deleteItem(item);
+    this.items = this.cart.getItems();
+    // this.refreshCart();
+  }
+  // getInfoUser() {
+  //   if (this.localstorage.getCurrentUser() === true && this.localstorage.userType==='user') {
+  //     this.successLogin = false;
+  //     this.userInfologin = this.localStorage.userJson;
+  //     let data = {
+  //       mobile: this.userInfologin['mobile']
+  //     };
+  //     this.authService.onfindUser(data).subscribe((response) => {
+  //       if (response['success'] === true) {
+  //         this.localStorage.saveCurrentUser(JSON.stringify(response['data']));
+  //         this.userInfologin = this.localStorage.userJson;
+  //         this.userInfo.id = this.userInfologin['_id'];
+  //         this.userInfo.firstName = this.userInfologin['firstName'];
+  //         this.userInfo.lastName = this.userInfologin['lastName'];
+  //         this.userInfo.state = this.userInfologin['state'];
+  //         this.userInfo.city = this.userInfologin['city'];
+  //         this.userInfo.mobile = this.userInfologin['mobile'];
+  //         this.userInfo.phone = this.userInfologin['phone'];
+  //         this.userInfo.address = this.userInfologin['address'];
+  //         this.userInfo.postalCode = this.userInfologin['postalCode'];
+  //       }
+  //     });
+  //   }
+  // }
 }
